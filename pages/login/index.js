93bb29cc6070e8336bba2be8d7dd370ed0be169f
premshetty <premenaga@gmail.com>
2022-05-27 00:15:05 +0530

    mobile login

---
+++ pages/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from '../../components/login/LoginPage'
+
+function index() {
+  return (
+    <div>
+        <LoginPage />
+    </div>
+  )
+}
+
+export default index
