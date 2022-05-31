a504e05deabe01750950b82b365e9756a917efd0
premshetty <premenaga@gmail.com>
2022-06-01 00:25:22 +0530

    lists page init

---
+++ components/list/Lists.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Search from './Search'
+
+function Lists() {
+  return (
+    <div className="flex flex-col gap-11">
+      <Navbar />
+      <Search />
+    </div>
+  )
+}
+
+export default Lists
---
+++ pages/lists/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lists from '../../components/list/Lists'
+
+function index() {
+  return (
+    <div>
+      <Lists />
+    </div>
+  )
+}
+
+export default index
